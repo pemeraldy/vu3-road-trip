41cc8dc472032ff4f2f430e091476546dec95bf4
pemeraldy <pemeraldy@gmail.com>
2021-03-14 17:20:28 +0100

    list item rendered

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,14 @@
   <router-view/>
 </template>
 
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'App'
+  })
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <items-list-component :items="items" />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+<script lang="ts" >
+import { defineComponent} from 'vue';
+import ItemsListComponent from '@/components/items/ItemsListComponent.vue';
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  components:{
+    ItemsListComponent
   },
-});
+  setup(){
+    const items: any[] =[
+      {id: 1, name:'item 1'},
+      {id: 2, name:'item 2'},
+      {id: 3, name:'item 3'},
+      {id: 4, name:'item 4'},
+      {id: 5, name:'item 5'},
+    ]
+    return{ items}
+  }
+})
 </script>
+
+<style>
+
+</style>
